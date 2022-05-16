879b588d412fae50e87b033dbc5c4c0e9f00e0b3
Sebastien Grazzini <grazzini@amazon.com>
2022-05-16 21:02:22 +0200

    road to v1

--- iot-things-library-infra/iot_things_library_infra/iot_things_library_infra_stack.py
+++ iot-things-library-infra/iot_things_library_infra/iot_things_library_infra_stack.py
@@ -5,6 +5,7 @@ from aws_cdk import (
     aws_iam as iam,
     aws_amplify_alpha as amplify,
     SecretValue,
+    custom_resources,
     CfnOutput,
     CfnParameter
 )
@@ -28,4 +29,28 @@ class IotThingsLibraryInfraStack(Stack):
 
         myAmplifyApp.grant_principal.add_managed_policy(iam.ManagedPolicy.from_aws_managed_policy_name('AdministratorAccess-Amplify'))
 
-        myMaster = myAmplifyApp.add_branch("master") # `id` will be used as repo branch name
+        myMaster = myAmplifyApp.add_branch("master",stage="PRODUCTION") # `id` will be used as repo branch name
+        
+        #
+        # Triggering Amplify build and deployment right after app creation
+        #
+        # https://docs.aws.amazon.com/cdk/api/v2/python/aws_cdk.custom_resources/AwsCustomResource.html
+        myBuildTrigger = custom_resources.AwsCustomResource(self, 'Amplify_Build_Trigger',
+            policy=custom_resources.AwsCustomResourcePolicy.from_sdk_calls(
+                resources=custom_resources.AwsCustomResourcePolicy.ANY_RESOURCE
+            ),
+            on_create=custom_resources.AwsSdkCall(
+                service='Amplify',
+                action='startJob',
+                physical_resource_id=custom_resources.PhysicalResourceId.of('app-build-trigger'),
+                parameters={
+                    "appId": myAmplifyApp.app_id,
+                    "branchName": myMaster.branch_name,
+                    "jobType": 'RELEASE',
+                    "jobReason":'Auto Start build',
+                }
+            )
+        )
+
+        #last we write the output
+        CfnOutput(self, "AppDomain", value=myAmplifyApp.default_domain)
